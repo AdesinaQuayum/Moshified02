c30bd03384fd72293acbacef4b1bdbb659f4484c
AdesinaQuayum <quayumadesina@gmail.com>
2026-04-24 15:02:07 +0100

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Portifilo from './Component/Portifilo'
+// import Hosting from './Component/Hosting'
+
+function App() {
+
+  return (
+    <>
+      <Portifilo/>
+      {/* <Hosting/> */}
+    </>
+  )
+}
+
+export default App
